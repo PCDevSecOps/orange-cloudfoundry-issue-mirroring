c921cdb2b966ef0b4060cb7ee22d710fd905d71a
Guillaume Berche <guillaume.berche@orange.com>
2024-02-21 20:43:04 +0100

    Normalize index with leading zero as to benefit from lexicographical sorting

--- download-issues.bash
+++ download-issues.bash
@@ -7,7 +7,7 @@ skip_existing_issue=true
 #useful to only refresh the recent issues
 recent_issues_limit=30
 #potentially slow down to avoid reaching gh rate limit
-sleep_seconds=5
+#sleep_seconds=5
 
 #See https://github.com/cli/cli/issues/3098#issuecomment-802842606 for sorting
 last_issue=$(gh issue -R ${repo} list -L 1 --json number  --search sort:created-desc --state all | jq -r .[].number)
@@ -21,19 +21,21 @@ recent_issues=$(gh issue -R ${repo} list -L ${recent_issues_limit} --json number
 issues=${recent_issues}
 issues=${all_issues}
 for i in ${issues}; do
-  if [[ "${skip_existing_issue}" == "true" && -d issues/$i ]]; then
-    echo "skipping existing $i"
+  # credit https://stackoverflow.com/a/8789815/1484823
+  normalized_index=$(printf "%04d" ${i})
+  if [[ "${skip_existing_issue}" == "true" && -d issues/${normalized_index} ]]; then
+    echo "skipping existing ${normalized_index}"
   else
-    echo "synching $i"
-    mkdir -p issues/$i
-    gh issue -R ${repo} view $i --json title,author,closed,number,labels,milestone,createdAt,state,url | yq --prettyPrint . > issues/$i/meta.yaml
-    gh issue -R ${repo} view $i --json body | jq -r .body > issues/$i/body.md
-    comment_file=issues/$i/comments.md
-    gh issue -R ${repo} view $i --json comments | jq -r  ".comments[] | \"\ncreatedAt: \" + .createdAt + \"\nauthor.login: \" + .author.login + \"\nbody:\n\" + .body " > ${comment_file}
+    echo "synching ${normalized_index}"
+    mkdir -p issues/${normalized_index}
+    gh issue -R ${repo} view ${normalized_index} --json title,author,closed,number,labels,milestone,createdAt,state,url | yq --prettyPrint . > issues/${normalized_index}/meta.yaml
+    gh issue -R ${repo} view ${normalized_index} --json body | jq -r .body > issues/${normalized_index}/body.md
+    comment_file=issues/${normalized_index}/comments.md
+    gh issue -R ${repo} view ${normalized_index} --json comments | jq -r  ".comments[] | \"\ncreatedAt: \" + .createdAt + \"\nauthor.login: \" + .author.login + \"\nbody:\n\" + .body " > ${comment_file}
     if ! [ -s ${comment_file} ]; then
       rm ${comment_file}
     else
-      echo "some comments in $i"
+      echo "some comments in ${normalized_index}"
     fi
     if [ -n "${sleep_seconds}" ]; then
       echo "sleeping ${sleep_seconds} seconds"
